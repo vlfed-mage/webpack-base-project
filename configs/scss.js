0b7fe3b404d39bc3bccd61ad37c30dba975ccc24
Vladyslav <gkorchun@gmail.com>
2021-10-24 12:10:10 +0300

    Webpack initial configuration

---
+++ configs/scss.js
@@ -0,0 +1,11 @@
+module.exports = {
+    test: /\.s[ac]ss$/,
+    use: [
+        // Creates `style` nodes from JS strings
+        "style-loader",
+        // Translates CSS into CommonJS
+        "css-loader",
+        // Compiles Sass to CSS
+        "sass-loader",
+    ],
+};
