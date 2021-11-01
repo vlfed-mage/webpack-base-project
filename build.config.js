fce5b32d945c3a1900854bff1bcda4ef89a7d7fc
Vladyslav <gkorchun@gmail.com>
2021-11-01 16:04:53 +0200

    set image-minifier configs

--- build.config.js
+++ build.config.js
@@ -1,10 +1,20 @@
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+const images = require('./configs/images');
+
+const plugins = [images.imageMinPlugin];
+
 module.exports = {
     mode: 'production',
     target: 'web',
+    performance: {
+        maxAssetSize: 500000
+    },
     optimization: {
         splitChunks: {
             chunks: 'all'
         },
         // runtimeChunk: 'single'
     },
+    plugins,
 }
--- configs/images.js
+++ configs/images.js
@@ -1,4 +1,25 @@
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
 const { fileName } = require('../helpers/file');
+
+module.exports.imageMinPlugin = new ImageMinimizerPlugin({
+
+    // https://web.dev/use-imagemin-to-compress-images/
+    // https://webpack.js.org/plugins/image-minimizer-webpack-plugin/
+
+    severityError: 'warning',
+    minimizerOptions: {
+
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+
+        plugins: [
+            ['gifsicle', { interlaced: true }],
+            ['mozjpeg', { progressive: true}],
+            ['pngquant', { quality: [0.45, 0.60], speed: 10}],
+        ],
+    },
+});
 
 module.exports.loader = {
     test: /\.(png|jpe?g|gif|svg)$/,
