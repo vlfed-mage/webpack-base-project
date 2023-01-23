c3f2411eafa565b678edf0dddf01003c78d4d2c0
Valdo <gkorchun@gmail.com>
2023-01-23 15:21:12 +0200

    Extra change base indents view

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,8 +2,12 @@ import React, {Component} from 'react';
 
 export default class App extends Component {
     render() {
-        return pug`
-			.some-app
-		`
+        return <div className='react__app'>sample</div>
+
+        // you can use pug(jade) inside react in this way
+
+        // return pug`
+        //     .some-app
+        // `
     }
 }
