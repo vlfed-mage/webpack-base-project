2844aa956e36132f8653da79e61f594cdca7bc16
Valdo <gkorchun@gmail.com>
2023-01-30 15:56:11 +0200

    Set json-server for working with local API

--- dev.config.js
+++ dev.config.js
@@ -4,11 +4,32 @@ const routes = require('./configs/routes');
 module.exports = {
     devServer: {
         contentBase: routes.build,
+        watchContentBase: true,
         compress: true,
         hot: true,
         open: true,
         port: 9000,
-        historyApiFallback: true, // https://ui.dev/react-router-cannot-get-url-refresh#webpack--development
+        historyApiFallback: true, // https://ui.dev/react-router-cannot-get-url-refresh#webpack--development,
+        proxy: {
+            '/api': 'http://localhost:5000',
+        },
+        stats: {
+            colors: true,
+            hash: false,
+            version: false,
+            timings: false,
+            assets: false,
+            chunks: false,
+            modules: false,
+            reasons: false,
+            children: false,
+            source: true,
+            errors: true,
+            errorDetails: false,
+            warnings: false,
+            publicPath: false,
+            overlay: true,
+        },
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
