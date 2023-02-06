7be3096d767f68796652204385c08e323373099b
Valdo <gkorchun@gmail.com>
2023-02-06 15:48:47 +0200

    Add emotion/css library

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,27 @@
 import React, {Component} from 'react';
 
+import { css } from '@emotion/css';
+
+const appStyles = css`
+    margin: 50px auto;
+    width: 100%;
+    max-width: 1440px;
+
+    .container {
+        background: #1d1e26;
+        border: 4px solid #9580ff;
+        border-radius: 6px;
+        padding: 25px;
+    }
+`;
+
 export default class App extends Component {
     render() {
-        return <div className='react__app'>sample1</div>
+        return (
+            <div className={ appStyles }>
+                sample1
+            </div>
+        );
 
         // you can use pug(jade) inside react in this way
 
